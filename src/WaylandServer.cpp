9b804383bdda6889d8ccbe5eaea39ae228b5b67d
William W Wold <wilcoze@gmail.com>
2017-08-03 19:40:07 -0700

    filled out implementation of seat a bit more, and now it segfaults

--- src/WaylandServer.cpp
+++ src/WaylandServer.cpp
@@ -44,15 +44,16 @@ struct WaylandServerImpl: WaylandServerBase
 			
 			struct wl_compositor_interface compositorInterface {
 				
+				// create surface
 				+[](wl_client * client, wl_resource * resource, uint32_t id)
 				{
-					if (instance->verbose)
-						cout << "compositorCreateSurfaceCallback called" << endl;
+					cout << "compositorCreateSurfaceCallback called" << endl;
 				},
+				
+				// create region
 				+[](wl_client * client, wl_resource * resource, uint32_t id)
 				{
-					if (instance->verbose)
-						cout << "compositorCreateRegionCallback called" << endl;
+					cout << "compositorCreateRegionCallback called" << endl;
 				}
 			};
 			
@@ -72,6 +73,7 @@ struct WaylandServerImpl: WaylandServerBase
 				cout << "shellBindCallback called" << endl;
 			
 			struct wl_shell_interface shellInterface {
+				// get shell surface
 				+[](wl_client * client, wl_resource * resource, uint32_t id, wl_resource * surface) {
 					
 					cout << "shellInterface called" << endl;
@@ -86,8 +88,6 @@ struct WaylandServerImpl: WaylandServerBase
 		
 		auto seatBindCallback = +[](wl_client * client, void * data, uint32_t version, uint32_t id)
 		{
-			cout << "seatBindCallback called" << endl;
-			/*
 			if (instance == nullptr)
 			{
 				logError("oh shit, there's no WaylandServerImpl instance!");
@@ -95,20 +95,43 @@ struct WaylandServerImpl: WaylandServerBase
 			}
 			
 			if (instance->verbose)
-				cout << "shellBindCallback called" << endl;
+				cout << "seatBindCallback called" << endl;
 			
-			struct wl_shell_interface shellInterface {
-				+[](wl_client * client, wl_resource * resource, uint32_t id, wl_resource * surface) {
-					
-					cout << "shellInterface called" << endl;
-					//struct wl_resource *shell_surface = wl_resource_create (client, &wl_shell_surface_interface, 1, id);
-					//wl_resource_set_implementation (shell_surface, &shell_surface_interface, NULL, NULL);
+			struct wl_seat_interface seatInterface
+			{
+				// get pointer
+				+[](struct wl_client *client, struct wl_resource *resource, uint32_t id)
+				{
+					cout << "seat interface get pointer called" << endl;
+					/*
+					struct wl_resource *pointer = wl_resource_create (client, &wl_pointer_interface, 1, id);
+					wl_resource_set_implementation (pointer, &pointer_interface, NULL, NULL);
+					get_client(client)->pointer = pointer;
+					*/
+				},
+				// get keyboard
+				+[](struct wl_client *client, struct wl_resource *resource, uint32_t id)
+				{
+					cout << "seat interface get keyboard called" << endl;
+					/*struct wl_resource *keyboard = wl_resource_create (client, &wl_keyboard_interface, 1, id);
+					wl_resource_set_implementation (keyboard, &keyboard_interface, NULL, NULL);
+					get_client(client)->keyboard = keyboard;
+					int fd, size;
+					backend_get_keymap (&fd, &size);
+					wl_keyboard_send_keymap (keyboard, WL_KEYBOARD_KEYMAP_FORMAT_XKB_V1, fd, size);
+					//close (fd);
+					*/
+				},
+				// get touch
+				+[](struct wl_client *client, struct wl_resource *resource, uint32_t id)
+				{
+					cout << "seat interface get touch called" << endl;
 				}
 			};
 			
-			wl_resource * resource = wl_resource_create(client, &wl_shell_interface, 1, id);
-			wl_resource_set_implementation(resource, &shellInterface, nullptr, nullptr);
-			*/
+			wl_resource * seat = wl_resource_create(client, &wl_seat_interface, 1, id);
+			wl_resource_set_implementation(seat, &seatInterface, nullptr, nullptr);
+			wl_seat_send_capabilities(seat, WL_SEAT_CAPABILITY_POINTER | WL_SEAT_CAPABILITY_KEYBOARD);
 		};
 		
 		// create global objects
