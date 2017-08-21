28c7afdc92e6a96f6d8ec0db705292b53e6f7321
William W Wold <wilcoze@gmail.com>
2017-08-21 09:54:37 -0700

    stripped surface code from WaylandServer

--- wayland/WaylandServer.cpp
+++ wayland/WaylandServer.cpp
@@ -1,6 +1,7 @@
 #include "../main/utils.h"
 #include "../opengl/Texture.h"
 #include "WaylandServer.h"
+#include "WaylandSurface.h"
 
 #include <wayland-server-protocol.h>
 #include <wayland-server.h>
@@ -12,24 +13,15 @@
 // no clue what this means
 typedef void (*PFNGLEGLIMAGETARGETTEXTURE2DOESPROC) (GLenum target, EGLImage image);
 
-struct SurfaceData
-{
-	struct wl_resource * surface;
-	struct wl_resource * buffer;
-	Texture texture;
-};
-
 struct WaylandServerImpl: WaylandServerBase
 {
-	WaylandServerImpl(bool verboseIn)
+	WaylandServerImpl(VerboseToggle verboseIn)
 	{
 		verbose = verboseIn;
 		
 		message("starting Wayland server");
 		
 		// get function pointers
-		eglBindWaylandDisplayWL = (PFNEGLBINDWAYLANDDISPLAYWL)eglGetProcAddress("eglBindWaylandDisplayWL");
-		eglQueryWaylandBufferWL = (PFNEGLQUERYWAYLANDBUFFERWL)eglGetProcAddress("eglQueryWaylandBufferWL");
 		glEGLImageTargetTexture2DOES = (PFNGLEGLIMAGETARGETTEXTURE2DOESPROC)eglGetProcAddress("glEGLImageTargetTexture2DOES");
 		
 		display = wl_display_create();
@@ -49,137 +41,7 @@ struct WaylandServerImpl: WaylandServerBase
 				+[](wl_client * client, wl_resource * resource, uint32_t id)
 				{
 					message("compositor interface create surface callback called");
-					assertInstance();
-					//struct surface *surface = calloc (1, sizeof(struct surface));
-					instance->surfaceInterface = {
-						// surface destroy
-						+[](wl_client * client, wl_resource * resource)
-						{
-							message("surface interface surface destroy callback called (not yet implemented)");
-						},
-						// surface attach
-						+[](wl_client * client, wl_resource * resource, wl_resource * buffer, int32_t x, int32_t y)
-						{
-							message("surface interface surface attach callback called");
-							SurfaceData * surface = (SurfaceData *)wl_resource_get_user_data(resource);
-							surface->buffer = buffer;
-						},
-						// surface damage
-						+[](wl_client * client, wl_resource * resource, int32_t x, int32_t y, int32_t width, int32_t height)
-						{
-							message("surface interface surface damage callback called (not yet implemented)");
-						},
-						// surface frame
-						+[](wl_client * client, wl_resource * resource, uint32_t callback)
-						{
-							message("surface interface surface frame callback called (not yet implemented)");
-							//struct surface *surface = wl_resource_get_user_data (resource);
-							//surface->frame_callback = wl_resource_create (client, &wl_callback_interface, 1, callback);
-						},
-						// surface set opaque region
-						+[](wl_client * client, wl_resource * resource, wl_resource * region)
-						{
-							message("surface interface surface set opaque region callback called (not yet implemented)");
-						},
-						// surface set input region
-						+[](wl_client * client, wl_resource * resource, wl_resource * region)
-						{
-							message("surface interface surface set input region callback called (not yet implemented)");
-						},
-						// surface commit
-						+[](wl_client * client, wl_resource * resource)
-						{
-							message("surface interface surface commit callback called");
-							
-							assertInstance();
-								
-							SurfaceData * surfaceData = (SurfaceData *)wl_resource_get_user_data(resource);
-							
-							//struct surface *surface = wl_resource_get_user_data (resource);
-							EGLint texture_format;
-							auto display = GLXContextManagerBase::instance->getDisplay();
-							auto buffer = surfaceData->buffer;
-							
-							// query the texture format of the buffer
-							bool idkThisVarMeans = instance->eglQueryWaylandBufferWL(
-									display,
-									buffer,
-									EGL_TEXTURE_FORMAT,
-									&texture_format
-								);
-							
-							if (idkThisVarMeans) {
-								EGLint width, height;
-								instance->eglQueryWaylandBufferWL(display, buffer, EGL_WIDTH, &width);
-								instance->eglQueryWaylandBufferWL(display, buffer, EGL_WIDTH, &height);
-								EGLAttrib attribs = EGL_NONE;
-								EGLImage image = eglCreateImage(display, EGL_NO_CONTEXT, EGL_WAYLAND_BUFFER_WL, buffer, &attribs);
-								//texture_delete(&surface->texture);
-								surfaceData->texture.loadFromEGLImage(image, V2i(width, height));
-								//texture_create_from_egl_image (&surface->texture, width, height, image);
-								eglDestroyImage (display, image);
-							}
-							else {
-								struct wl_shm_buffer * shmBuffer = wl_shm_buffer_get(buffer);
-								uint32_t width = wl_shm_buffer_get_width(shmBuffer);
-								uint32_t height = wl_shm_buffer_get_height(shmBuffer);
-								void * data = wl_shm_buffer_get_data(shmBuffer);
-								//texture_delete(&surface->texture);
-								//texture_create(&surface->texture, width, height, data);
-								surfaceData->texture.loadFromData(data, V2i(width, height));
-							}
-							wl_buffer_send_release (buffer);
-							//redraw_needed = 1;
-						},
-						// surface set buffer transform
-						+[](wl_client * client, wl_resource * resource, int32_t transform)
-						{
-							message("surface interface surface set buffer transform callback called (not yet implemented)");
-						},
-						// surface set buffer scale
-						+[](wl_client * client, wl_resource * resource, int32_t scale)
-						{
-							message("surface interface surface set buffer scale callback called (not yet implemented)");
-						},
-					};
-					
-					auto deleteSurface = [](wl_resource * resource)
-					{
-						message("delete surface callback called (not yet implemented)");
-						assertInstance();
-						int i = 0;
-						for (; i < (int)instance->surfaces.size(); i++)
-						{
-							if (instance->surfaces[i]->surface == resource)
-								break;
-						}
-						if (i < (int)instance->surfaces.size())
-						{
-							instance->surfaces.erase(instance->surfaces.begin() + i);
-						}
-						
-						/*
-						struct surface *surface = wl_resource_get_user_data (resource);
-						wl_list_remove (&surface->link);
-						if (surface == active_surface) active_surface = NULL;
-						if (surface == pointer_surface) pointer_surface = NULL;
-						free (surface);
-						redraw_needed = 1;
-						*/
-					};
-					
-					struct wl_resource * surface = wl_resource_create(client, &wl_surface_interface, 3, id);
-					auto surfaceData = shared_ptr<SurfaceData>(new SurfaceData
-						{
-							(struct wl_resource *)surface,
-							(struct wl_resource *)nullptr,
-							Texture(VERBOSE_OFF),
-						});
-					message("adding surface!");
-					instance->surfaces.push_back(surfaceData);
-					wl_resource_set_implementation(surface, &instance->surfaceInterface, &*surfaceData, +deleteSurface);
-					//surface->client = get_client (client);
-					//wl_list_insert (&surfaces, &surface->link);
+					WaylandSurface(client, id, verbose);
 				},
 				
 				// create region
@@ -356,18 +218,6 @@ struct WaylandServerImpl: WaylandServerBase
 	{
 		wl_event_loop_dispatch(eventLoop, 0);
 		wl_display_flush_clients(display);
-		//message("drawing " + to_string(surfaces.size()) + " textures...");
-		for (auto i: surfaces)
-		{
-			i->texture.draw();
-		}
-		//if (needsRedraw)
-		//{
-		//	needsRedraw = false;
-		//}
-		//else {
-		//	
-		//}
 	}
 	
 	inline static void message(string msg)
@@ -389,12 +239,10 @@ struct WaylandServerImpl: WaylandServerBase
 	
 	static WaylandServerImpl * instance;
 	
-	static bool verbose;
+	static VerboseToggle verbose;
 	
 	// function pointers that need to be retrieved at run time. This is Cs sad, pathetic attempt at duck typing.
 	PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glEGLImageTargetTexture2DOES = nullptr;
-	PFNEGLBINDWAYLANDDISPLAYWL eglBindWaylandDisplayWL = nullptr;
-	PFNEGLQUERYWAYLANDBUFFERWL eglQueryWaylandBufferWL = nullptr;
 	
 	wl_display * display = nullptr;
 	
@@ -405,17 +253,12 @@ struct WaylandServerImpl: WaylandServerBase
 	struct wl_shell_interface shellInterface;
 	struct wl_seat_interface seatInterface;
 	struct wl_pointer_interface pointerInterface;
-	
-	struct wl_surface_interface surfaceInterface;
-	vector<shared_ptr<SurfaceData>> surfaces;
-	
-	bool needsRedraw = false;
 };
 
 WaylandServerImpl * WaylandServerImpl::instance = nullptr;
-bool WaylandServerImpl::verbose = false;
+VerboseToggle WaylandServerImpl::verbose = VERBOSE_OFF;
 
-WaylandServer WaylandServerBase::make(bool verbose)
+WaylandServer WaylandServerBase::make(VerboseToggle verbose)
 {
 	if (WaylandServerImpl::instance != nullptr)
 	{
