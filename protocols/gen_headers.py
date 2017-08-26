f72b574fc6608f32517e756ae1e4c66483144e5b
William W Wold <wilcoze@gmail.com>
2017-08-26 01:43:11 -0400

    added python script to automatically call wayland-scanner and generate all protocols

---
+++ protocols/gen_headers.py
@@ -0,0 +1,13 @@
+#! /bin/python3
+from os import path, listdir;
+from sys import argv;
+from subprocess import run;
+if len(argv) < 2:
+	print('please specify the path for the protocols')
+	exit(1)
+elif len(argv) > 2:
+	print('too many arguments')
+base_path = argv[1]
+xml_files = [ path.join(base_path, xml) for xml in listdir(base_path) if xml.endswith('.xml') ]
+for xml in xml_files:
+	run(['wayland-scanner', '--include-core-only', 'server-header', xml, xml.rsplit('.', 1)[0] + '.h'])
