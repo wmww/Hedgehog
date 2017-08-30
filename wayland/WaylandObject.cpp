19d724fd1f6ed19677b2e90addc66bff937fbc58
William W Wold <wilcoze@gmail.com>
2017-08-30 05:18:46 -0400

    simplified WaylandObject header by removing map from it

--- wayland/WaylandObject.cpp
+++ wayland/WaylandObject.cpp
@@ -1,34 +1,34 @@
 #include "WaylandObject.h"
 #include <wayland-server.h>
 
-std::unordered_map<uint32_t, shared_ptr<WaylandObject>> WaylandObject::map;
+std::unordered_map<uint32_t, shared_ptr<WaylandObject>> waylandObjectMap;
 
 void WaylandObject::WaylandObjectSetup(wl_client * client, uint32_t id, const wl_interface * interface, int version, const void * implStruct)
 {
 	wl_resource * resource = wl_resource_create(client, interface, version, id);
 	wl_resource_set_implementation(resource, implStruct, nullptr, deleteObj);
-	map[id] = shared_from_this();
+	waylandObjectMap[id] = shared_from_this();
 }
 
 void WaylandObject::deleteObj(wl_resource * resource)
 {
 	assert(resource != nullptr);
-	auto iter = map.find(resource->object.id);
-	if (iter == map.end())
+	auto iter = waylandObjectMap.find(resource->object.id);
+	if (iter == waylandObjectMap.end())
 	{
 		warning("deleteObj called for ID not in map");
 	}
 	else
 	{
-		map.erase(iter);
+		waylandObjectMap.erase(iter);
 	}
 }
 
 shared_ptr<WaylandObject> WaylandObject::getWaylandObjectFrom(wl_resource * resource)
 {
 	assert(resource != nullptr);
-	auto iter = map.find(resource->object.id);
-	if (iter == map.end())
+	auto iter = waylandObjectMap.find(resource->object.id);
+	if (iter == waylandObjectMap.end())
 	{
 		warning("getWaylandObjectFrom called for ID not in map");
 		return shared_ptr<WaylandObject>(nullptr);
--- wayland/WaylandObject.h
+++ wayland/WaylandObject.h
@@ -6,8 +6,6 @@ class WaylandObject: public std::enable_shared_from_this<WaylandObject>
 {
 public:
 	
-	static std::unordered_map<uint32_t, shared_ptr<WaylandObject>> map;
-	
 	void WaylandObjectSetup(wl_client * client, uint32_t id, const wl_interface * interface, int version, const void * implStruct);
 	
 	static void deleteObj(wl_resource * resource);
