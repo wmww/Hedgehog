883cd2bbdb7b213661ccddd2059032e87e99c933
William W Wold <wilcoze@gmail.com>
2017-09-17 11:48:34 -0700

    removed shaders folder (as it looks like we wont need external shaders for a while

--- scene/WindowInterface.h
+++ scene/WindowInterface.h
@@ -11,19 +11,6 @@ public:
 	virtual weak_ptr<InputInterface> getInputInterface() = 0;
 	
 	Texture texture;
-	
-	// to implement:
-	/*
-	void pointerMotion(V2i newPos)
-	{
-		warning(FUNC + " not yet implemented");
-	}
-	
-	weak_ptr<InputInterface> getInputInterface()
-	{
-		warning(FUNC + " not yet implemented");
-	}
-	*/
 };
 
 #include "Scene.h" // must be at bottom for include order
